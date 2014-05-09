c5e2ae0716565cf9451da31aee4b744cea39a93e
jimenez <jimenez@yahoo-inc.com>
2014-05-08 18:01:17 -0700

    Fixed jshint errors.

--- index.js
+++ index.js
@@ -15,19 +15,26 @@ var nopt = require('nopt'),
     args = opts.argv.remain;
 
 function main() {
+    // Saving cwd in order to switch to this directory and then go back.
+    // This allows yeoman to find generator-mojito.
+    var cwd = process.cwd();
+    process.chdir(__dirname);
+
     // Create yeoman environment object.
     var env = require('yeoman-generator')();
     env.alias(/^([^:]+)$/, '$1:all');
     env.alias(/^([^:]+)$/, '$1:app');
     env.lookup();
 
+    process.chdir(cwd);
+
     // Remove the first argument which is always 'create'.
     args.splice(0, 1);
 
     if (args[0] === 'help') {
         // Support 'mojito create help' as the help menu.
-        args[0] = 'mojito'
-        opts['help'] = true;
+        args[0] = 'mojito';
+        opts.help = true;
     } else if (args.length === 0) {
         // No arguments after 'mojito create' is the equivalent of 'yo mojito'.
         args[0] = 'mojito';
